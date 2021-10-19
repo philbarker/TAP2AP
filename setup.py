86de59fcd61646af18f590248b3347a8bb37ff51
Phil Barker <phil.barker@pjjk.co.uk>
2021-10-19 18:32:07 +0100

    fixing typos

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from distutils.core import setup
 
 setup(name='TAP2AP',
-      version='0.1',
+      version='0.1.1',
       description='Convert DC TAP to python AP',
       author='Phil Barker',
       author_email='phil.barker@pjjk.co.uk',
-      url='https://github.com/philbarker/AP',
-      packages=['TAP2AP'],
+      url='https://github.com/philbarker/TAP2AP',
+      packages=['tap2ap'],
      )
