042a87fba0e2bb38704e4d9bf500abf96bd536ba
Phil Barker <phil.barker@pjjk.co.uk>
2022-02-03 14:19:41 +0000

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='TAP2AP',
-      version='0.1.3',
+      version='0.1.4',
       description='Convert DC TAP to python AP',
       author='Phil Barker',
       author_email='phil.barker@pjjk.co.uk',
